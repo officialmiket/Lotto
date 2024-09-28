d81984e5ec7d46783a6b35b0bd7bd390e7782a43
officialmiket <76247566+officialmiket@users.noreply.github.com>
2024-09-28 19:34:34 +0300

    Added more randomizer constraints and file write
    Added more randomizer constraints and file write alot more optimizers and better stuff

--- lotto.java
+++ lotto.java
@@ -1,22 +1,32 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class lotto {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many rows do you want: ");
         int rows = scanner.nextInt();
 
+        System.out.println("Would you like to print to file your results. yes?");
+        int writers = scanner.nextInt();
+        Set<Integer> generatedNumbers = new HashSet<>();
+
+        int length = 7;
+        int[] numbers = new int[length];
+
         while (rows > 0) {
          
             Random rd = new Random();
-            int length = 7;
             int min = 1;
             int max = 40;
-            int[] numbers = new int[length];
-    
+
             for (int x = 0; x < length; x++) {
                 numbers[x] = rd.nextInt(max - min) + min;
             }
@@ -27,11 +37,31 @@ public class lotto {
             int[] spnumbers = new int[splength];
 
             for (int x = 0; x < splength; x++) {
-                spnumbers[x] = rd.nextInt(spmax - spmin) + spmin;
+                int randomNumber;
+                // Keep generating a new number until we find one that is not already in the set
+                do {
+                    randomNumber = rd.nextInt(spmax - spmin) + spmin;
+                } while (generatedNumbers.contains(randomNumber));
+                
+                spnumbers[x] = randomNumber;
+                generatedNumbers.add(randomNumber);
             }
-    
+
+            if (writers == 1) {
+                File file = new File("Lotto.txt");
+                FileWriter fw = new FileWriter(file, true);
+                fw.write("Numbers" +Arrays.toString(numbers) + "Special numbers: "+ Arrays.toString(spnumbers) +"\n");
+                fw.close();
+            } else {}
+
             System.out.println(Arrays.toString(numbers) + (Arrays.toString(spnumbers)));
             rows--;
         }
     }
 }
+
+/*        if (writers.equals("yes" + "y" + "Y" + "Yes")) {
+} else {
+    //fw.close();
+    scanner.close();
+} */
